2c4a1d1fafcc2b157191cc7661507a9ea51c06ea
shantanuk7 <shantanukulkarni365@gmail.com>
2023-07-17 23:38:27 +0530

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Upload from "../../components/Upload";
-import Sidebar from "../../components/Sidebar";
 export default function Home() {
   return (
     <main>
       <div className="mainBox">
-        <Sidebar/>
+        
         <div className="workSpace flex-all-center">
           <Upload />
         </div>
-        
       </div>
     </main>
   );
